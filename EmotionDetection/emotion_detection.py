bbfe37d0c6fb5565a4159064853d19159cc332e9
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-05-25 18:37:57 +0200

    partial feature / Tasks 6, 7 and 8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,7 +8,7 @@ def emotion_detector(text_to_analyze):
     response = requests.post(URL, json = input_json, headers=header)
     if response.status_code == 200:
         formatted_response = json.loads(response.text)
-    elif response.status_code == 500:
+    elif response.status_code == 400:
         formatted_response = {
                             'anger': None,
                             'disgust': None, 
@@ -20,20 +20,23 @@ def emotion_detector(text_to_analyze):
     return formatted_response
 
 def emotion_predictor(detected_text):
-    emotions = detected_text['emotionPredictions'][0]['emotion']
-    anger = emotions['anger']
-    disgust = emotions['disgust']
-    fear = emotions['fear']
-    joy = emotions['joy']
-    sadness = emotions['sadness']
-    max_emotion = max(emotions, key=emotions.get)
-    #max_emotion_score = emotions[max_emotion]
-    formated_dict_emotions = {
-                            'anger': anger,
-                            'disgust': disgust,
-                            'fear': fear,
-                            'joy': joy,
-                            'sadness': sadness,
-                            'dominant_emotion': max_emotion
-                            }
-    return formated_dict_emotions
+    if all(value is None for value in detected_text.values()):
+        return detected_text
+    if detected_text['emotionPredictions'] is not None:
+        emotions = detected_text['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        max_emotion = max(emotions, key=emotions.get)
+        #max_emotion_score = emotions[max_emotion]
+        formated_dict_emotions = {
+                                'anger': anger,
+                                'disgust': disgust,
+                                'fear': fear,
+                                'joy': joy,
+                                'sadness': sadness,
+                                'dominant_emotion': max_emotion
+                                }
+        return formated_dict_emotions
